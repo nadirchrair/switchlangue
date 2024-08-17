86afbba8c08eaa4da074902b6011fb34cb8bc4ec
nadir chrair <chrairmohamednadir@gmail.com>
2024-08-17 13:19:21 +0100

    GTSC final version

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,17 +1,17 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from '../src/pages/GTSC/Content/en.json';
-//import fr from '../src/pages/GCU/Content/fr.json'
+import fr from '../src/pages/GTSC/Content/fr.json'
 i18n
   .use(initReactI18next)
   .init({
     resources: {
       en: { translation: en },
-     // fr:{ translation: fr }
+      fr:{ translation: fr }
 
     },
-    lng: 'en', // Default language
-    fallbackLng: 'en',
+    lng: 'fr', // Default language
+    fallbackLng: 'fr',
     interpolation: {
       escapeValue: false, // React already does escaping
     },
